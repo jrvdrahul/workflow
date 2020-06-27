3fbdf55d32bf8a10bf05a8e15a58d9f18792b1b3
rahul <rahul.v@coppermobile.com>
2020-06-27 17:37:37 +0530

    login page ui added

---
+++ workflow/src/component/Register.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Label,
+  Row,
+  Col,
+} from 'reactstrap';
+import {
+  NotificationContainer,
+  NotificationManager,
+} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: false,
+    };
+  }
+
+  login = (e) => {
+    e.preventDefault();
+    if (this.state.number == '' || this.state.number == undefined) {
+      NotificationManager.error('Number is required', 'Error!');
+      return false;
+    } else if (this.state.password == '' || this.state.password == undefined) {
+      NotificationManager.error('Password is required', 'Error!');
+      return false;
+    }
+  };
+
+  render() {
+    if (this.state.success) {
+      return <Redirect to="dashboard/index" />;
+    }
+    return (
+      <>
+        <section id="login" className="perfect-center">
+          <div className="login">
+            <Card className="shadow border-0">
+              <CardBody className="px-lg-5 py-lg-5">
+                <h3 className="text-center">Sign up</h3>
+                <Form onSubmit={this.login}>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText></InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Email"
+                        type="text"
+                        onChange={(event) =>
+                          this.setState({ number: event.target.value })
+                        }
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText></InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Password"
+                        type="password"
+                        onChange={(event) =>
+                          this.setState({ password: event.target.value })
+                        }
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText></InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Confirm Password"
+                        type="password"
+                        onChange={(event) =>
+                          this.setState({ password: event.target.value })
+                        }
+                      />
+                    </InputGroup>
+                  </FormGroup>
+
+                  <div className="text-center ">
+                    <Button
+                      className="mt-4"
+                      color="secondary"
+                      type="submit"
+                      disabled={!this.state.number || !this.state.password}
+                    >
+                      Sign up
+                    </Button>
+                  </div>
+                  <div className="mt-4">
+                    {/* Already have an Account ? <Link to="register">Login here</Link> */}
+                  </div>
+                </Form>
+              </CardBody>
+            </Card>
+          </div>
+          <NotificationContainer />
+        </section>
+      </>
+    );
+  }
+}
+
+export default Register;
